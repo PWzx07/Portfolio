4f861ebc0f0c6bef44fbbd58b204940d7daa46ed
Pedro W. <Pazevedodejesus@gmail.com>
2024-09-05 19:51:41 -0300

    primeiro commit

---
+++ PORTFOLIO.js
@@ -0,0 +1,8 @@
+function GIBLE(){
+    alert("Metas: Consolidar o Conhecimento em Python, Desenvolver um Projeto Pessoal em HTML/CSS/JavaScript, Familiarizar-se com Ferramentas de Desenvolvimento")
+}
+
+function pichu(){
+    alert("Objetivos: Construir um Portfólio Profissional, Preparar-se para Estágios ou Primeiros Empregos, Aprender Tecnologias Avançadas")
+    }
+    
